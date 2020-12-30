d276771e3049a01d639a02e274785007501c4ea5
ciulloeugenio <65494496+ciulloeugenio@users.noreply.github.com>
2020-12-30 01:34:32 +0100

    Update Pedaliera 3 Celle HX711.ino

--- Pedaliera 3 Celle HX711.ino
+++ Pedaliera 3 Celle HX711.ino
@@ -46,19 +46,19 @@ Lo sketch per la Pedaliera è stato configurato per ricevere il segnale da 3 amp
 
 Collegare come segue:
 Cella Acceleratore:
-VCC -> 5v
+VCC -> VCC
 GND -> GND
 DOUT -> PIN2
 Clock -> PIN3
 
 Cella Freno:
-VCC -> 5v
+VCC -> VCC
 GND -> GND
 DOUT -> PIN4
 Clock -> PIN5
 
 Cella Frizione:
-VCC -> 5v
+VCC -> VCC
 GND -> GND
 DOUT -> PIN6
 Clock -> PIN7
@@ -82,17 +82,17 @@ Premere i pedali, e impostare i valori qui di seguito per tarare il minimo e il 
 */
 
 // Valori Costanti
-const long ACCMIN = -3585;
-const long ACCMAX = 4174;
-const long BRMIN = 3204;
-const long BRMAX = 23678;
-const long CLMIN = 5628;
-const long CLMAX = 7638;
+const long ACCMIN = -32767;
+const long ACCMAX = 32767;
+const long BRMIN = -32767;
+const long BRMAX = 32767;
+const long CLMIN = -32767;
+const long CLMAX = 32767;
 
 // Fattore di Calibrazione
-const double calibration_factor1 = 5;
-const double calibration_factor2 = 100;
-const double calibration_factor3 = 25;
+const long calibration_factor1 = 10;   //Sostituire questo valore di modo che il valore raw della cella sia nel range tra -32767 e 32767
+const long calibration_factor2 = 10;  //Sostituire questo valore di modo che il valore raw della cella sia nel range tra -32767 e 32767
+const long calibration_factor3 = 10; //Sostituire questo valore di modo che il valore raw della cella sia nel range tra -32767 e 32767
 
 
 // Dettaglio HID Joystick
@@ -111,7 +111,7 @@ HX711 scale2;
 HX711 scale3;
 
 void setup() {
-//  Serial.begin(57600); //Decommentare per aprire il debug
+  Serial.begin(9600); //Decommentare per aprire il debug
   scale1.begin(LOADCELL_DOUT_PIN1, LOADCELL_SCK_PIN1);
   scale2.begin(LOADCELL_DOUT_PIN2, LOADCELL_SCK_PIN2);
   scale3.begin(LOADCELL_DOUT_PIN3, LOADCELL_SCK_PIN3);
@@ -126,16 +126,16 @@ void setup() {
 
 void loop() {
     long reading1 = scale1.get_units();
-//    Serial.print("HX711 reading1: \t");    //Decommentare per aprire il debug
-//    Serial.print(reading1);     //Decommentare per aprire il debug
+    Serial.print("Acceleratore: \t");    //Decommentare per aprire il debug
+    Serial.print(reading1);     //Decommentare per aprire il debug
     Joystick.setRxAxis(reading1);
     long reading2 = scale2.get_units();
-//    Serial.print("\t\t HX711 reading2: \t");    //Decommentare per aprire il debug
-//     Serial.print(reading2);      //Decommentare per aprire il debug
+    Serial.print("\t\t Freno: \t");    //Decommentare per aprire il debug
+     Serial.print(reading2);      //Decommentare per aprire il debug
     Joystick.setRyAxis(reading2);
     long reading3 = scale3.get_units();
-//    Serial.print("\t\t HX711 reading3: \t");      //Decommentare per aprire il debug
-//    Serial.println(reading3);     //Decommentare per aprire il debug
+    Serial.print("\t\t Frizione: \t");      //Decommentare per aprire il debug
+    Serial.println(reading3);     //Decommentare per aprire il debug
     Joystick.setRzAxis(reading3);
     delay(5);
 }
